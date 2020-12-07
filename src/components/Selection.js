9965eaf96eff757174af76b6161c2e61703b55ca
Varsharani Ambi <68917117+varsha-ambi@users.noreply.github.com>
2020-12-07 14:00:39 +0530

    d s

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+import React, { useState } from "react";
 
+const Selection = (props) => {
+  const [sty, upStyle] = useState({ background: "" });
+  const up = () => {
+    console.log("UP");
+    console.log(props.ik);
+    props.applyColor(upStyle);
+  };
+  return (
+    <div onClick={up} className="fix-box" style={sty}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+};
+export default Selection;
